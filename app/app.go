30b4bb5bdf6b1a5e33df724f337e65477c45dc93
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-07-10 14:37:59 -0300

    Find DNS

--- app/app.go
+++ app/app.go
@@ -4,26 +4,31 @@ import (
 	"github.com/urfave/cli"
 	"log"
 	"fmt"
-	"net"
-	)
-
+	"net")
 
 //Build comand-line app
 func Build() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Comand line app in go"
 	app.Usage = "IP Resolver"
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name: "host",
+			Value: "google.com",
+		},
+	}
 	app.Commands = []cli.Command{
 		{
 			Name: "ip",
 			Usage: "Lookup ip address of a website.",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name: "host",
-					Value: "google.com",
-				},
-			},
+			Flags: flags,
 			Action: findIP,
+		},
+		{
+			Name: "servers",
+			Usage: "Find DNS of host",
+			Flags: flags,
+			Action: findServers,
 		},
 	}
 	return app
@@ -38,5 +43,17 @@ func findIP(c *cli.Context){
 	}
 	for _, ip := range ips{
 		fmt.Println(ip)
+	}
+}
+
+func findServers(c *cli.Context){
+	host := c.String("host")
+	fmt.Println("DNS for:",host)
+	servers, err := net.LookupNS(host) //name servers
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
