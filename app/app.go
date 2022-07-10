daeb4f2822d1d22ac890e9a16d7a865591d01748
jrschmidtt <junior.schmidt.99@gmail.com>
2022-07-10 14:10:27 -0300

    Find Host IP

--- app/app.go
+++ app/app.go
@@ -1,11 +1,42 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"github.com/urfave/cli"
+	"log"
+	"fmt"
+	"net"
+	)
+
 
 //Build comand-line app
 func Build() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Comand line app in go"
 	app.Usage = "IP Resolver"
+	app.Commands = []cli.Command{
+		{
+			Name: "ip",
+			Usage: "Lookup ip address of a website.",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "host",
+					Value: "google.com",
+				},
+			},
+			Action: findIP,
+		},
+	}
 	return app
+}
+
+func findIP(c *cli.Context){
+	host := c.String("host")
+	fmt.Println("IP's:",host)
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, ip := range ips{
+		fmt.Println(ip)
+	}
 }
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"command-line/app"
 	"os"
 	"log"
 )
 
 func main() {
-	fmt.Println("Entry point")
-
 	application := app.Build()
 	if err := application.Run(os.Args) ; err != nil {
 		log.Fatal(err)
